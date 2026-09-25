cbdd07baa1b2f2f5059be676748621830f007f60
matthew clark <matthewronaldjohnclark@gmail.com>
2026-09-25 13:17:55 +0100

    first commit

---
+++ traffic_with_pygame.py
@@ -0,0 +1,21 @@
+import pygame
+import time
+
+pygame = pygame.draw.rect()
+pygame = pygame.draw.circle()
+
+current_light = "red = stop"
+traffic_lights = ["red = stop", "amber = wait", "green = go"]
+while True:
+    if current_light == "red = stop":
+        print(traffic_lights[0])
+        time.sleep(5)
+        current_light = "amber = wait"
+    elif current_light == "amber = wait":
+        print(traffic_lights[1])
+        time.sleep(2)
+        current_light = "green = go"
+    elif current_light == "green = go":
+        print(traffic_lights[2])
+        time.sleep(3)
+        current_light = "red = stop"
